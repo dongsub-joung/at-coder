2d66f959fb2556babc82920abd13234199fcbc75
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-12-23 08:28:29 +0900

    런타임 에러

--- stpe/stpe01/q11/src/main.rs
+++ stpe/stpe01/q11/src/main.rs
@@ -1,4 +1,12 @@
+use std::io::{stdin, BufRead};
+
 fn main() {
-    // https://www.acmicpc.net/problem/11382
-    println!("Hello, world!");
+    let std= stdin();
+    let mut buf= std.lock().lines();
+    let input_v: Vec<i32>= buf.next().unwrap().unwrap()
+        .split_whitespace()
+        .map(|f| f.parse::<i32>().unwrap())
+        .collect();
+
+    println!("{}", input_v[0] + input_v[1] + input_v[2]);
 }
