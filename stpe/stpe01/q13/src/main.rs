f74ff575ee5b4891d6a6e35b3fd1e4b7c65cf121
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-12-23 09:18:47 +0900

    pass

---
+++ stpe/stpe01/q13/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //https://www.acmicpc.net/problem/10172
+    println!("|\\_/|");
+    println!("|q p|   /}}");
+    println!("( 0 )\"\"\"\\");
+    println!("|\"^\"`    |");
+    println!("||_/=\\\\__|")
+}
